eb56b3a78cf31ef4663aa23e03a7d27344174d3a
Diotheboss16 <diotheboss16@gmail.com>
2025-05-31 20:27:25 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("message").innerText = "Hello from JavaScript!";
-});
-console.log("JavaScript is running!");
+let box = document.getElementById("box");
+let position = 0;
+let speed = 2; // Pixels per frame
+
+function animate() {
+    if (position < window.innerWidth - 50) {
+        position += speed;
+        box.style.left = position + "px";
+        requestAnimationFrame(animate); // Calls itself for smooth animation
+    }
+}
+
+animate();
