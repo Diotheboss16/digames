ad447c4b0dda35ae7c6f76ecab0ef57de72d653d
Diotheboss16 <diotheboss16@gmail.com>
2025-05-31 20:20:52 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("message").innerText = "Hello from JavaScript!";
 });
+console.log("JavaScript is running!");
