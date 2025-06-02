e70161397d34a71b052df4b0d1c8da13984aa37c
Diotheboss16 <diotheboss16@gmail.com>
2025-06-02 11:44:58 +0700

    Update script.js

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ let speed = 2; // Pixels per frame
 let direction = 1; // 1 for moving right, -1 for moving left
 
 function animate() {
-    if (position >= window.innerWidth - 50 || position <= 0) {
-        direction *= -1; // Reverse direction
-    }
+    //if (position >= window.innerWidth - 50 || position <= 0) {
+    //    direction *= -1; // Reverse direction
+    //}
     position += speed * direction;
     box.style.transform = `translateX(${position}px)`;
     requestAnimationFrame(animate);
