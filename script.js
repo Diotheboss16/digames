f343e03cf80a9df93e8c6ac7b3a2b30f5841430b
Diotheboss16 <diotheboss16@gmail.com>
2025-06-02 12:11:12 +0700

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function animate() {
     position += speed * direction;
     box.style.transform = `translateX(${position}px)`;
     console.log("Been Here!");
-    console.log(position);
+    console.log("Pos: " + position);
     requestAnimationFrame(animate);
 }
 
