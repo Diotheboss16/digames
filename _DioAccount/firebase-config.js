3e06e8589e628998e749e5d8869c0e573dc75661
Dio <diotheboss16@gmail.com>
2026-01-04 17:01:53 +0700

    DioAccount

--- _DioAccount/firebase-config.js
+++ _DioAccount/firebase-config.js
@@ -1,18 +1,15 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
+// Firebase web app config (copy from Firebase Console).
+// IMPORTANT: do NOT import from "firebase/app" here (that's for bundlers/npm). This project
+// loads Firebase via the CDN in app.js.
+//
+// Hosting URL (GitHub Pages):
+//   https://diotheboss16.github.io/digames/_DioAccount/
+export const firebaseConfig = {
   apiKey: "AIzaSyCiDMH0zPVSkaA9WCtlHTbAYSssF6DFq1w",
   authDomain: "dio-account.firebaseapp.com",
   projectId: "dio-account",
   storageBucket: "dio-account.firebasestorage.app",
   messagingSenderId: "20379226632",
-  appId: "1:20379226632:web:bea009b5dc717aea5537bd"
+  appId: "1:20379226632:web:bea009b5dc717aea5537bd",
 };
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
 
